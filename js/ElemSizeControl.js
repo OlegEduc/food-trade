602c926b9bd54fc746cd228a8538b0a36bac7b76
Oleg <76013020+OlegEduc@users.noreply.github.com>
2024-04-19 22:17:03 +0300

    Add files via upload

--- js/ElemSizeControl.js
+++ js/ElemSizeControl.js
@@ -1,9 +1,26 @@
 // Внутренний размер окна — это ширина и высота области просмотра (вьюпорта).
 // Объект window предоставляет свойства innerWidth и innerHeight:
 
-const windowInnerWidth = window.innerWidth
-const windowInnerHeight = window.innerHeight
+window.addEventListener("resize", () => {
+  //   const windowInnerWidth = window.innerWidth;
+  
+  const wrapper = document.querySelector(".wrapper");
+  const header = document.querySelector(".header");
+  const content = document.querySelector(".content");
+  const footer = document.querySelector(".footer");
+  const windowInnerHeight = getHeightWindow();
+  wrapper.style.height = windowInnerHeight + "px";
 
-const wrapper = document.querySelector('.wrapper')
-wrapper.style.height =  windowInnerHeight + 'px';
-content.style.height = (windowInnerHeight - footer.style.height - header.style.height) + 'px';
+  content.style.height =
+    windowInnerHeight - footer.style.height - header.style.height + "px";
+});
+
+//  Возвращает высоту экрана 
+function getHeightWindow() {
+    let windowInnerHeight;
+    if (window.innerHeight > window.innerWidth) {
+      return Math.max(window.innerHeight, window.innerWidth);
+    } else {
+      return Math.min(window.innerHeight, window.innerWidth);
+    }
+}
