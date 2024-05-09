cb9d70e1da8288f854d11ef3791486a1ba243038
Oleg <76013020+OlegEduc@users.noreply.github.com>
2024-05-09 12:40:51 +0300

    Update ElemSizeControl.js

--- js/ElemSizeControl.js
+++ js/ElemSizeControl.js
@@ -45,8 +45,8 @@ export function setHeightUserWindow() {
   const footer = document.querySelector(".footer");
   const windowInnerHeight = getHeightWindow();
   // wrapper.style.height = windowInnerHeight + "px";
-  content.style.maxHeight =
-    windowInnerHeight - footer.style.height - header.style.height + "px";
+  // content.style.maxHeight =
+  //   windowInnerHeight - footer.style.height - header.style.height + "px";
 
   // if (window.innerWidth > 600) {
   //   footer.querySelector(".search").style.display = "flex";
