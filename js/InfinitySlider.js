7f65d692a3fc350e6e5d406cb65f7beae5e0a644
Oleg <76013020+OlegEduc@users.noreply.github.com>
2025-07-07 21:42:11 +0300

    Update InfinitySlider.js

--- js/InfinitySlider.js
+++ js/InfinitySlider.js
@@ -6,12 +6,12 @@ let slides = [
   "./image/action/ПечивоКрапкаВаніль.jpg",
   "./image/action/ПечивоКрапкаКакао.jpg",
   "./image/action/ПечивоКрапкаКанапкаПломбір.jpg",
-  "./image/action/МаксиФрукт-3л-баннер-Общий.jpg",
+  "./image/action/МаксиФрукт-3л-баннер-Общ-Дост.jpg",
 
   "./image/action/ПечивоКрапкаКанапкаШоколад.jpg",
   "./image/action/ПечивоПодільськеАрахіс.jpg",
   "./image/action/ПечивоПодільськеПряжМолоко.jpg",
-   "./image/action/МаксиФрукт-3л-баннер-Общий.jpg",
+   "./image/action/МаксиФрукт-3л-баннер-Общ-Дост.jpg",
 ];
 
 let slider = [];
